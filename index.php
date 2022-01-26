ad5e16ab0e37dbc3543b1353e74688926337cba6
asad <asadtarki49@gmail.com>
2022-01-26 11:24:59 +0530

    updated

--- index.php
+++ index.php
@@ -19,11 +19,11 @@
         </nav>
     </header>
     <main>
-        <div class="form-container" id="form">
-            <form action="" method="">
+        <div class="form-container"  id="form">
+            <form action="" enctype="multipart/form-data" method="post">
                 <h3>Upload files</h3>
                 <div class="inputblock" id="ib0">
-                    <input type="file" class="file"/>
+                    <input type="file" class="file" name="file"/>
                 </div>
                 <div class="inputblock">
                     
@@ -33,12 +33,12 @@
 
                 <div class="inputblock ib2" id="ib2">
                     
-                    <textarea class="message" placeholder="  "></textarea>
+                    <textarea class="message" placeholder="  " name="message"></textarea>
                     <label  class="mylabel">message</label>
                 </div>
                 <div class="inputblock ib3" id="ib3">
                     
-                    <input type="submit" value="generate link" id="linkbutton"/>
+                    <input type="submit" value="generate link" id="linkbutton" name="submit"/>
                 </div>
                
             </form>
@@ -46,4 +46,20 @@
     </main>
 </body>
 <script src="script.js"></script>
+
 </html>
+
+<?php
+$con=mysqli_connect("localhost","root","","quickshare") ;
+if(isset($_REQUEST['submit']))
+{
+    $title=$_REQUEST['title'];
+    $message=$_REQUEST['message'];
+    $img=$_FILES["file"]["name"];
+    echo $img;
+    // echo "$title";
+}
+    
+
+
+?>
